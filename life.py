46a781783336144c5d376c716257042da5f387bd
Seth T <seth10@users.noreply.github.com>
2017-08-13 18:00:08 -0400

    Use curses for better visualization

--- life.py
+++ life.py
@@ -1,10 +1,12 @@
 import random
+import curses
+import time
 
-def  print2D(mainList):
-    print "Printing world"
-    for subList in mainList:
-        #print subList
-        print ["O" if element else " " for element in subList]
+def print2D(world):
+    for i, row in enumerate(world):
+        for j, element in enumerate(row):
+            pad.addch(i, j, "O" if element else " ")
+    pad.refresh(0,0, 0,0, len(world), len(world[0]))
 
 def iterate(today):
     tomorrow = [[False for _ in range(len(today[0]))] for _ in range(len(today))]
@@ -15,14 +17,17 @@ def iterate(today):
                 if (n,m) != (0,0): # don't count self
                     if today[(i+n)%len(today)][(j+m)%len(today)]: livingNeighbors += 1
             tomorrow[i][j] = 2 <= livingNeighbors <= 3
-            #tomorrow[i][j] = livingNeighbors
     return tomorrow
 
 if __name__ == "__main__":
-    SIZE = 10
-    world = [[random.random() < 0.5 for _ in range(SIZE)] for _ in range(SIZE)]
-    print2D(world)
-    world = iterate(world)
-    print2D(world)
-    world = iterate(world)
-    print2D(world)
+    try:
+        SIZE = 10
+        stdscr = curses.initscr()
+        pad = curses.newpad(SIZE, SIZE+1)
+        world = [[random.random() < 0.5 for _ in range(SIZE)] for _ in range(SIZE)]
+        for _ in range(10):
+            world = iterate(world)
+            print2D(world)
+            time.sleep(0.5)
+    finally:
+        curses.endwin()
