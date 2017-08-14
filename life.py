fc1a20a1cd4195d2dbe6093c98e21673ca09f193
Seth T <seth10@users.noreply.github.com>
2017-08-13 22:50:39 -0400

    Monitor for stable condition and print number of iterations taken

--- life.py
+++ life.py
@@ -14,7 +14,7 @@ def iterate(today):
     tomorrow = make2DList(len(today), len(today[0]))
     for i,row in enumerate(tomorrow):
         for j,col in enumerate(row):
-            livingNeighbors = 0
+            livingNeighbors = 0 # TODO: use reduce
             for n,m in [(x,y) for x in [-1,0,1] for y in [-1,0,1]]:
                 if (n,m) != (0,0): # don't count self
                     if today[(i+n)%len(today)][(j+m)%len(today)]: livingNeighbors += 1
@@ -22,17 +22,24 @@ def iterate(today):
     return tomorrow
 
 if __name__ == "__main__":
-    SIZE = 10
-    ITERATIONS = 20
-    DELAY = 0.5
+    SIZE = 7
+    MAX_ITERATIONS = 100000
+    DELAY = 0.2
     try:
         locale.setlocale(locale.LC_ALL, '')
         stdscr = curses.initscr()
         pad = curses.newpad(SIZE, SIZE+1)
-        world = make2DList(SIZE, SIZE, lambda: random.random() < 0.5)
-        for _ in range(ITERATIONS):
-            world = iterate(world)
-            print2D(world)
+        history = [make2DList(SIZE, SIZE, lambda: random.random() < 0.5)]
+        while len(history) < MAX_ITERATIONS:
+            history.append(iterate(history[-1]))
+            print2D(history[-1])
             time.sleep(DELAY)
+            if len(history) >= 3 and history[-1] == history[-3]: # TODO: search for longer stable patterns
+                pad.addstr(0,0, "Stable")
+                pad.refresh(0,0, 0,0, 1,6)
+                time.sleep(1)
+                curses.endwin()
+                print "Stable after {} iterations".format(len(history))
+                break
     finally:
         curses.endwin()
