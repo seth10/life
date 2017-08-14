099b50e421a4362eab373b644aab3d5e8346b5a5
Seth T <seth10@users.noreply.github.com>
2017-08-14 17:16:11 -0400

    Don't use a pad
    Ah much better/cleaner/clearer

--- life.py
+++ life.py
@@ -4,8 +4,8 @@ import curses, locale
 def print2D(world):
     for i, row in enumerate(world):
         for j, element in enumerate(row):
-            pad.addstr(i, j, u'\u25CB'.encode('utf-8') if element else " ")
-    pad.refresh(0,0, 0,0, len(world), len(world[0]))
+            stdscr.addstr(i, j, u'\u25CB'.encode('utf-8') if element else " ")
+    stdscr.refresh()
 
 def make2DList(rows, columns, generator = lambda: False):
     return [[generator() for _ in range(rows)] for _ in range(columns)]
@@ -20,12 +20,11 @@ def iterate(today):
 
 if __name__ == "__main__":
     SIZE = 7
-    DELAY = 0.01
+    DELAY = 0.1
     MAX_ITERATIONS = 2*int(1/DELAY) # let the simulation run up to 2 seconds
     try:
         locale.setlocale(locale.LC_ALL, '')
         stdscr = curses.initscr()
-        pad = curses.newpad(SIZE, SIZE+1)
         history = [make2DList(SIZE, SIZE, lambda: random.random() < 0.5)]
         while len(history) < MAX_ITERATIONS:
             history.append(iterate(history[-1]))
@@ -34,13 +33,10 @@ if __name__ == "__main__":
             if len(history) >= 3 and history[-1] == history[-3]: # TODO: search for longer stable patterns
                 break
         if len(history) < MAX_ITERATIONS:
-            resultMessage = "Stable after {} iterations".format(len(history))
+            stdscr.addstr(SIZE, 0, "Stable after {} iterations".format(len(history)))
         else:
-            resultMessage = "Did not stabilize after {} iterations".format(MAX_ITERATIONS)
-        pad.resize(SIZE+1, len(resultMessage)+1)
-        pad.addstr(SIZE, 0, resultMessage)
-        pad.refresh(SIZE,0, SIZE,0, SIZE,len(resultMessage))
+            stdscr.addstr(SIZE, 0, "Did not stabilize after {} iterations".format(MAX_ITERATIONS))
         curses.flushinp() # discard any input received while simulation was running
-        pad.getch() # wait for any keypress
+        stdscr.getch() # wait for any keypress
     finally: # even if there were any exceptions, be sure curses stops so the prompt returns
         curses.endwin()
