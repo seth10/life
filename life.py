8160cbf7896c3f45abedd6f8db7e41122410db5d
Seth T <seth10@users.noreply.github.com>
2017-08-29 22:53:05 -0400

    Start parallelization
    Working on #1
    Basically, last commit this took just shy of 5 minutes and now it takes 3 minutes. Underwhelming to say the least.
    
    Before I used a variable `_` to ignore the index in the list comprehension. I could've moved this variable to the simulate function's argument list, but instead I just added *args to ignore any further positional arguments, more generalized.

--- life.py
+++ life.py
@@ -1,4 +1,4 @@
-import random
+import random, multiprocessing, functools
 
 def make2DList(rows, columns, generator = lambda: False):
     return [[generator() for _ in range(rows)] for _ in range(columns)]
@@ -18,7 +18,7 @@ def iterate(today):
             #   - Each cell with three neighbors becomes populated.
     return tomorrow
 
-def simulate(size, startPercent):
+def simulate(size, startPercent, *args):
     history = [make2DList(size, size, lambda: random.random() < startPercent)]
     while history[-1] not in history[:-1]:
         history.append(iterate(history[-1]))
@@ -31,6 +31,6 @@ if __name__ == "__main__":
     for SIZE in range(5, 10):
         #for START_PERCENT in map(lambda n: n/10.0, range(1,10)):
             START_PERCENT = 0.9
-            # TODO: parallelize
-            results = [simulate(SIZE, START_PERCENT) for _ in range(TRIALS)]
+            pool = multiprocessing.Pool(processes=4)
+            results = pool.map(functools.partial(simulate, SIZE, START_PERCENT), range(TRIALS))
             print "{0}x{0} grid with {1:.0f}% initially alive: {2}% stable".format(SIZE, START_PERCENT*100, sum(results)*100.0/len(results))
