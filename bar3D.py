22c7edb4b9834dbd03611e9616ec123e731b5241
Seth T <seth10@users.noreply.github.com>
2017-08-30 12:16:41 -0400

    Shift stuff around in 3D bar graph example for clarity

--- bar3D.py
+++ bar3D.py
@@ -3,7 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-result=[
+data = [
     [0,  8, 19, 36, 37, 24, 16,  5, 1, 0, 0],
     [0, 15, 37, 37, 39, 33, 34,  8, 0, 0, 0],
     [0, 16, 30, 41, 37, 24, 27,  7, 0, 0, 0],
@@ -11,36 +11,37 @@ result=[
     [0, 36, 61, 64, 65, 62, 53, 26, 0, 1, 0]
 ]
 
-result = np.array(result, dtype=np.int)
+result = np.array(data, dtype=np.int)
 
-fig = plt.figure(figsize=(8,5), dpi=150)
+fig = plt.figure(figsize=(6,5), dpi=150)
 ax = fig.add_subplot(111, projection='3d')
 
-xlabels = np.array(['0','10','20','30','40','50','60','70','80','90','100'])
-xpos = np.arange(xlabels.shape[0])
-ylabels = np.array(['5x5', '6x6', '7x7', '8x8', '9x9'])
-ypos = np.arange(ylabels.shape[0])
+xpos = np.arange(len(data[0]))
+ypos = np.arange(len(data))
 
 xposM, yposM = np.meshgrid(xpos, ypos, copy=False)
-
 zpos = result.ravel()
 
-dx=0.5
-dy=0.5
-dz=zpos
+x = xposM.ravel()
+y = yposM.ravel()
+z = zpos*0
 
-ax.w_xaxis.set_ticks(xpos + dx/2.)
-ax.w_xaxis.set_ticklabels(xlabels)
+dx = 0.5
+dy = 0.5
+dz = zpos
 
-ax.w_yaxis.set_ticks(ypos + dy/2.)
-ax.w_yaxis.set_ticklabels(ylabels)
+ax.w_xaxis.set_ticks(xpos + dx/2)
+ax.w_xaxis.set_ticklabels(['0','10','20','30','40','50','60','70','80','90','100'])
+
+ax.w_yaxis.set_ticks(ypos + dy/2)
+ax.w_yaxis.set_ticklabels(['5x5', '6x6', '7x7', '8x8', '9x9'])
 
 ax.set_title('Initial cell population and grid size vs stabilization/eradication')
 ax.set_xlabel('Percentage of cells initially alive')
 ax.set_ylabel('Grid size')
 ax.set_zlabel('Percentage of simulations stabilized')
 
-values = np.linspace(0.2, 1., xposM.ravel().shape[0])
+values = np.linspace(0.2, 1, x.shape[0])
 colors = cm.rainbow(values)
-ax.bar3d(xposM.ravel(), yposM.ravel(), dz*0, dx, dy, dz, color=colors)
+ax.bar3d(x, y, z, dx, dy, dz, colors)
 plt.show()
